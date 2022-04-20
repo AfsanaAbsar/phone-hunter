1aa85ceb603af2e3edc33367135ae60a7f12423b
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-04-20 13:29:29 +0600

    responsive done

--- js/app.js
+++ js/app.js
@@ -76,19 +76,34 @@ const showDetails = data => {
     details.textContent = '';
 
     const div = document.createElement('div')
-    div.classList.add('phone-details-card')
+    div.classList.add('col')
     div.innerHTML = `
-        <div class="p-5 w-25 mx-auto bg-light ">
-        <img src="${data.image}" class="img-fluid p-5" alt="...">
-        <h4>${data.name}</h4>
-        <h5>Brand : ${data.brand} </h5>
-        <p>${release()}</p>
-        <h4>Features</h4>
-        <p><span class="details-title">Chipset :</span> ${data.mainFeatures.chipSet}</p>
-        <p><span class="details-title">Displaysize :</span> ${data.mainFeatures.displaySize}</p>
-        <p><span class="details-title">Memory :</span> ${data.mainFeatures.memory}</p>
-        <p><span class="details-title">Sensors :</span> ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]}</p>
-        </div>
+    
+    <div class="card h-100 text-center">
+    <img src="${data.image}" class="img-fluid p-5" alt="...">
+<div class="card-body mt-5">
+<h4>${data.name}</h4>
+<h5>Brand : ${data.brand} </h5>
+<p>${release()}</p>
+<h4>Features</h4>
+<p><span class="details-title">Chipset :</span> ${data.mainFeatures.chipSet}</p>
+<p><span class="details-title">Displaysize :</span> ${data.mainFeatures.displaySize}</p>
+<p><span class="details-title">Memory :</span> ${data.mainFeatures.memory}</p>
+<p><span class="details-title">Sensors :</span> ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]}</p>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+   
+
+
         `
 
     details.appendChild(div);
