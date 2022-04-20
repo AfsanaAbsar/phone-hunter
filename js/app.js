f79398e92e498685ddf253d62ef45ab39cfcfe71
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-04-20 13:07:08 +0600

    final

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
-
+// spinner function
 const toggleSpinner = displayStyle => {
     document.getElementById('spinner').style.display = displayStyle;
 }
 const searchPhone = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
+    //clear input value
+    searchInput.value = "";
 
-    console.log(searchText);
-
-    searchInput.value = ""
-
-    toggleSpinner('block')
+    toggleSpinner('block');
     // load api
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(data => showResult(data.data.slice(0, 20)));
 }
-
+//display search result
 const showResult = phones => {
-    const searchResult = document.getElementById('search-result')
+    const searchResult = document.getElementById('search-result');
     searchResult.textContent = "";
-    console.log(searchResult);
+
 
     if (phones.length == 0) {
         document.getElementById('error-search').style.display = 'block';
@@ -31,22 +29,20 @@ const showResult = phones => {
 
         document.getElementById('error-search').style.display = 'none';
         phones.forEach(phone => {
-            console.log(phone);
-
 
             const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = `
-        <div class="card h-100 text-center">
-                <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
+                <div class="card h-100 text-center">
+                     <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
                 <div class="card-body mt-5">
-                        <h4 class="card-title">${phone.phone_name}</h4>
-                        <h5 class="card-title">Brand : ${phone.brand} </h5>
+                        <h4 class="card-title pb-2">${phone.phone_name}</h4>
+                        <h5 class="card-title pb-2">Brand : ${phone.brand} </h5>
                  
-                        <button onclick="loadPhoneDetails('${phone.slug}')" class="details-btn ps-4 pe-4 pt-2 pb-2">click</button>
+                        <button onclick="loadPhoneDetails('${phone.slug}')" class="details-btn ps-4 pe-4 pt-2 pb-2">Details</button>
                 </div>
-            </div>
-        `
+                </div>
+             `
             searchResult.appendChild(div)
         });
 
@@ -54,6 +50,8 @@ const showResult = phones => {
 
     toggleSpinner('none')
 }
+
+//show phone details
 
 const loadPhoneDetails = phoneId => {
     console.log(phoneId);
@@ -64,7 +62,18 @@ const loadPhoneDetails = phoneId => {
 }
 
 const showDetails = data => {
+
+    const release = () => {
+        if (data.releaseDate.length == 0) {
+            return "Release Date Not Found"
+        }
+        else {
+            return data.releaseDate;
+        }
+    }
+
     const details = document.getElementById('phone-details');
+    details.textContent = '';
 
     const div = document.createElement('div')
     div.classList.add('phone-details-card')
@@ -73,31 +82,15 @@ const showDetails = data => {
         <img src="${data.image}" class="img-fluid p-5" alt="...">
         <h4>${data.name}</h4>
         <h5>Brand : ${data.brand} </h5>
-        <p>${data.releaseDate}</p>
+        <p>${release()}</p>
         <h4>Features</h4>
-        <p>Chipset : ${data.mainFeatures.chipSet}</p>
-        <p>Displaysize : ${data.mainFeatures.displaySize}</p>
-        <p>Memory : ${data.mainFeatures.memory}</p>
-        <p>Sensors : ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]}</p>
-       
-        
-
+        <p><span class="details-title">Chipset :</span> ${data.mainFeatures.chipSet}</p>
+        <p><span class="details-title">Displaysize :</span> ${data.mainFeatures.displaySize}</p>
+        <p><span class="details-title">Memory :</span> ${data.mainFeatures.memory}</p>
+        <p><span class="details-title">Sensors :</span> ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]}</p>
         </div>
         `
 
     details.appendChild(div);
 }
 
-// mainFeatures:
-// chipSet: "Apple A15 Bionic (5 nm)"
-// displaySize: "5.4 inches, 71.9 cm2 (~85.1% screen-to-body ratio)"
-// memory: "128GB 4GB RAM, 256GB 4GB RAM, 512GB 4GB RAM"
-
-
-// sensors: Array(6)
-// 0: "Face ID"
-// 1: "accelerometer"
-// 2: "gyro"
-// 3: "proximity"
-// 4: "compass"
-// 5: "barometer"
